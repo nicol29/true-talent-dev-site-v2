4760e085158ca2fb583ea24af119e4036aafd883
nicol29 <nicolascondrea@gmail.com>
2023-07-09 18:45:19 +0100

    Finish functionality of the hire-page

--- javascript/hireStaff.js
+++ javascript/hireStaff.js
@@ -45,3 +45,110 @@ const manageArticle = (e) => {
 expandButtons.forEach(button => button.addEventListener("click", (e) => manageArticle(e)));
 
 
+
+const modalBg = document.querySelector(".modal-bg");
+const hireButtons = document.querySelectorAll("article > button");
+const serviceInput = document.querySelector(".non-editable-row > input");
+
+const openModal = (e) => {
+  const chosenService = e.currentTarget.parentNode.querySelector("h3").innerText;
+  const currentStatus = (modalBg.dataset.active === "true");
+
+  if (!currentStatus) {
+    serviceInput.value = chosenService;
+    modalBg.dataset.active = true;
+    document.body.classList.add("disable-scroll");
+  }
+}
+
+const closeModal = (e) => {
+  const currentStatus = (modalBg.dataset.active === "true");
+
+  if (currentStatus) {
+    serviceInput.value = "";
+    modalBg.dataset.active = false;
+    document.body.classList.remove("disable-scroll");
+  }
+}
+
+hireButtons.forEach(button => button.addEventListener("click", (e) => openModal(e)));
+
+
+const closeButton = document.querySelector(".modal-leftside > img");
+
+closeButton.addEventListener("click", () => {
+  closeModal();
+});
+
+modalBg.addEventListener("click", (e) => {
+  if (e.target === modalBg) closeModal();
+});
+
+
+const searchBar = document.querySelector(".search-container > input");
+const searchIcon = document.querySelector(".search-container > img");
+const gridContainer = document.querySelector(".grid-container");
+const services = [...document.querySelector(".grid-container").children];
+const tagContainer = document.querySelector(".tag-container");
+
+
+searchBar.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    filterServices(e.target.value);
+
+    e.target.value = "";
+  }
+});
+
+searchIcon.addEventListener("click", () => filterServices(searchBar.value));
+
+const filterServices = (searchTerm) => {
+  const filteredServices = services.filter(service => {
+    const jobHeading = service.querySelector("h3").innerText;
+    const words = jobHeading.split(" ");
+    let found = false;
+
+    const shortenedWord = jobHeading.slice(0, searchTerm.length);
+    if (searchTerm.toLowerCase() === shortenedWord.toLowerCase()) found = true;
+
+    if (!found) {
+      words.forEach(word => {
+        const shortenedWord = word.slice(0, searchTerm.length);
+  
+        if (searchTerm.toLowerCase() === shortenedWord.toLowerCase()) found = true;
+      })
+    }
+
+    if (found) return service;
+  });
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  filteredServices.forEach(service => {
+    service.querySelector(".article-details").dataset.expanded = "false";
+    gridContainer.appendChild(service);
+  });
+
+  if (searchTerm !== "") {
+    tagContainer.querySelector("span").innerText = searchTerm;
+    tagContainer.dataset.active = "true";
+  } else {
+    tagContainer.dataset.active = "false";
+  }
+}
+
+tagContainer.querySelector("img").addEventListener("click", () => {
+  tagContainer.dataset.active = "false";
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  services.forEach(service => {
+    service.querySelector(".article-details").dataset.expanded = "false";
+    gridContainer.appendChild(service);
+  });
+});
+
